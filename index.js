4a3ab51845b1986d154b5b8e4c32ff1c98ad3e5b
David-McCaig <davidmccaig1@gmail.com>
2023-02-24 12:16:15 +1300

    create basic server. Add middleware

---
+++ index.js
@@ -0,0 +1,15 @@
+const port = 8080;
+const videosRouter = require('./routes/videosRouter');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+
+app.use('/videos', videosRouter)
+
+app.listen(port, () => {
+    console.log(`running at http://localhost:${port}`)
+})
