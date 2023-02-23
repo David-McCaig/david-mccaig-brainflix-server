6df7ffa7f22e238ead16de84d5196edf762ca8a4
David-McCaig <davidmccaig1@gmail.com>
2023-02-24 12:33:58 +1300

    create get all videos route

--- controllers/videosController.js
+++ controllers/videosController.js
@@ -1,1 +1,13 @@
+const knex = require('knex')(require('../knexfile'))
+const uuid = require("uuid");
 
+exports.getAllVideos = (_req, res) => {
+  knex('videos')
+    .select('id', 'title', 'channel', 'image')
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) =>
+      res.status(400).send(`Error retrieving Warehouses: ${err}`)
+    );
+};
---
+++ routes/videosRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const videosController = require('../controllers/videosController');
+
+router.get('/', videosController.getAllVideos);
+
+module.exports = router;
