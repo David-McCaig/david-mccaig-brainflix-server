4ba5e9b47c77af7c5bdceb5b54e476ba2b8a2896
David-McCaig <davidmccaig1@gmail.com>
2023-02-24 09:57:47 +1300

    create migration files and seeds

---
+++ migrations/a_20230123025410_videos.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = function (knex) {
+    return knex.schema.createTable('videos', (table) => {
+      table.uuid('id').primary();
+      table.string('title');  
+      table.string('channel');    
+      table.string('image');
+      table.string('description', 1000);
+      table.integer('views').defaultTo(0);
+      table.integer('likes').defaultTo(0);
+      table.string('duration').defaultTo(0);
+      table.timestamps(true, true);
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function (knex) {
+    return knex.schema.dropTable('videos');
+  };
---
+++ migrations/b_20230125025616_comments.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = function (knex) {
+    return knex.schema.createTable('comments', (table) => {
+      table.uuid('id').primary();
+      table
+        .uuid('videos_id')
+        .references('videos.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      table.string('name');
+      table.string('comment', 1000);
+      table.integer('likes').defaultTo(0);
+      table.timestamps(true, true);
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function (knex) {
+    return knex.schema.dropTable('comments');
+  };
